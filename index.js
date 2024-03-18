207558aef554be7c674c4c2e938c06db6119ae8b
Samuelmindyobiz <Samuelolatidoye@yahoo.com>
2024-03-18 13:37:03 +0100

    8000 server

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 import chalk from "chalk"
+import http from 'http'
+
+
+const host = 'localhost'
+const port = 8000
+
+const server = http.createServer((request, response) => {
+    response.end('<h1>hello World</h1>')
+})
+
+server.listen(port, host, () => console.log(chalk.red(`Server is running on http://${host}:${port}`)))
+
 console.log("hello world")
 
 
