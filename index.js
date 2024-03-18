3cac8c067c118647b5134a58fd4b0e833eb2b5a9
Samuelmindyobiz <Samuelolatidoye@yahoo.com>
2024-03-18 11:13:59 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+import chalk from "chalk"
+console.log("hello world")
+
+
+
+
+console.log(chalk.blue('Samuel'));
+console.log(chalk.green('Paul'));
+console.log(chalk.yellow('Alex'));
+console.log(chalk.red('Wara'));
+
+
+
+
+
