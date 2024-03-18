b14a28e4263bba334f329257171dd766951dfa97
Samuelmindyobiz <Samuelolatidoye@yahoo.com>
2024-03-18 16:07:49 +0100

    api call

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
 import chalk from "chalk"
 import http from 'http'
 import 'dotenv/config'
+import fs from 'fs'
+import axios from 'axios'
 
 
 const host = process.env.HOST
 const port = process.env.PORT
+ 
+fs.writeFile('data.txt', 'hello world', 'utf-8', (err) => {
+    if (err) throw err
+})
+
+fs.readFile('data.txt', 'utf-8', (err, data) => {
+    if (err) throw err
+
+    axios.get('http://www.google.com')
+  .then((response) => {
+    fs.writeFile('google.html', response.data, (err) => {
+      if (err) throw err;
+      console.log('Google HTML file created successfully!');
+    });
+  })
+  .catch((error) => {
+    console.error('Error fetching data from Google:', error);
+  });
+
+    console.log('Data read from the file:');
+    console.log(data);
+})
+
 
 console.log(process.env)
 
